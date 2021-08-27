05b2f54a02efbd093bb00b73c02240ac1caa969b
Antonio <antoni.martinelli@studenti.unina.it>
2021-08-27 11:07:25 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker system prune --force
 docker volume prune --force
-docker-compose up --build
+docker-compose up --build &
 sleep 15
 docker exec -ti service_01 /home/run.sh
 sleep 5
