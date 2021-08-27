1c4c3af3a88bec85611dda01cf0003871f0db3af
Antonio <antoni.martinelli@studenti.unina.it>
2021-08-27 11:07:04 +0200

    update start scripts

--- no_build_start.sh
+++ no_build_start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #docker system prune --force
 #docker volume prune --force
-docker-compose up --build
-#docker exec -ti service_01 /home/run.sh
+docker-compose up --build &
+sleep 15
+docker exec -ti service_01 /home/run.sh
+sleep 5
+corosync-cmapctl | grep members
--- start.sh
+++ start.sh
@@ -2,3 +2,7 @@
 docker system prune --force
 docker volume prune --force
 docker-compose up --build
+sleep 15
+docker exec -ti service_01 /home/run.sh
+sleep 5
+corosync-cmapctl | grep members
