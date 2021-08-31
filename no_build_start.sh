7069da0390caa26c32ad8a89fb7b1325c7596749
Antonio <antoni.martinelli@studenti.unina.it>
2021-08-31 10:33:31 +0200

    fix start script

--- no_build_start.sh
+++ no_build_start.sh
@@ -2,7 +2,7 @@
 #docker system prune --force
 #docker volume prune --force
 docker-compose up --build &
-sleep 15
+sleep 10
 docker exec -ti service_01 /home/run.sh
 sleep 5
-corosync-cmapctl | grep members
+docker exec -ti service_01 corosync-cmapctl | grep members
